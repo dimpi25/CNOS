53a79d1eebcaa03d14d5ff5d0f6d6f6e0038200e
dimpi <nannakadimpi2006@gmail.com>
2026-03-25 22:59:59 +0530

    Update Week10sequential.c
    Fixed nested loop structure for file allocation input.

--- Week10sequential.c
+++ Week10sequential.c
@@ -5,11 +5,13 @@ int n, start, size, i;
 printf("Enter number of files: ");
 scanf("%d", &n); 
 int allocated[100]={0};
-for(i=0; i<n; i++){
+for(i=0; i<n; i++)
+{
 printf("Enter start block and size of file %d: ", i+1); 
 scanf("%d %d", &start, &size);
 int j,flag=0;
-for (j=start; j<start + size; j++) {
+for (j=start; j<start + size; j++)
+{
 if(allocated[j]==1) 
 {
 printf("Block %d already allocated. Cannot allocate file %d\n", j, i+1); 
