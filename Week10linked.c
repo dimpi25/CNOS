84d47c99d993152834da6a443e1329b2977e397b
dimpi <nannakadimpi2006@gmail.com>
2026-03-25 22:44:35 +0530

    Update Week10linked.c

--- Week10linked.c
+++ Week10linked.c
@@ -1,5 +1,5 @@
-#include &lt;stdio.h&gt;
-#include &lt;stdlib.h&gt; 
+#include <stdio.h> 
+#include <stdlib.h> 
 struct Node 
 {
 int block; 
@@ -8,38 +8,40 @@ struct Node *next;
 int main() 
 {
 int n, size, i; 
-int free_blocks[100];
-for(i=0;i&lt;100;i++) free_blocks[i]=0; 
-printf(&quot;Enter number of files: &quot;);
-scanf(&quot;%d&quot;,&amp;n);
-for(i=0;i&lt;n;i++)
+int free_blocks[100]; 
+for(i=0;i<100;i++) free_blocks[i]=0; 
+printf("Enter number of files: ");
+scanf("%d",&n);
+for(i=0;i<n;i++)
 {
-printf(&quot;Enter size of file %d: &quot;, i+1);
-scanf(&quot;%d&quot;,&amp;size);
+printf("Enter size of file %d: ", i+1);
+scanf("%d",&size);
 struct Node *head=NULL, *temp, *newnode;
 int j, count=0;
-for(j=0;j&lt;100 &amp;&amp; count&lt;size;j++)
+for(j=0;j<100 && count<size;j++)
 {
-if(free_blocks[j]==0) 
+if(free_blocks[j]==0) // if block is free
 {
 
 newnode=(struct Node*)malloc(sizeof(struct Node)); 
-newnode-&gt;block=j; 
-newnode-&gt;next=NULL; 
+newnode->block=j; 
+newnode->next=NULL; 
 if(head==NULL) head=temp=newnode; 
-else { temp-&gt;next=newnode; temp=newnode; } 
+else { 
+temp->next=newnode; temp=newnode; 
+}
 free_blocks[j]=1; 
 count++;
 }
 }
-printf(&quot;File %d allocated blocks: &quot;, i+1);
+printf("File %d allocated blocks: ", i+1);
 temp=head;
 while(temp!=NULL)
 {
-printf(&quot;%d &quot;, temp-&gt;block); 
-temp=temp-&gt;next;
+printf("%d ", temp->block); 
+temp=temp->next; 
 }
-printf(&quot;\n&quot;);
+printf("\n");
 }
 return 0;
 }
